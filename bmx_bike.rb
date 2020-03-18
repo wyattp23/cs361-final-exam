f663f082721837b7744c57ec42de703ed168a239
Wyatt Pierce <piercewy@oregonstate.edu>
2020-03-18 11:24:39 -0700

    Added accessor to bmx_bike

--- bmx_bike.rb
+++ bmx_bike.rb
@@ -2,11 +2,13 @@ require_relative 'tail_pack'
 
 class BmxBike
 
+  attr_accessor :tail_pack, :weekly_rate, :daily_rate, :hourly_rate
+
   def initialize
     @tail_pack = TailPack.new
-    @weekly_price = 70
-    @daily_price = 20
-    @hourly_price = 5
+    @weekly_rate = 70
+    @daily_rate = 20
+    @hourly_rate = 5
   end
 
   def clean
@@ -15,22 +17,6 @@ class BmxBike
 
   def adjust_seat
     puts "Adjusting seat..."
-  end
-
-  def tail_pack
-    @tail_pack
-  end
-
-  def weekly_rate
-    @weekly_price
-  end
-
-  def daily_rate
-    @daily_price
-  end
-
-  def hourly_rate
-    @hourly_price
   end
 
 end
